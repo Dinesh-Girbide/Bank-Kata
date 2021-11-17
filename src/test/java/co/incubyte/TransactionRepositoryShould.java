4255ab96abb0bc5576edeb8ee60c08accc56adde
Dinesh-Girbide <ddgirbide@gmail.com>
2021-11-17 23:22:41 +0530

    Co-authored-by: Dinesh & Aslam ENH : - Added create_and_store_a_withdrawal_transaction() test in TransactionRepositoryShould.java with working logic completed          --create_and_store_a_deposit_transaction()

--- src/test/java/co/incubyte/TransactionRepositoryShould.java
+++ src/test/java/co/incubyte/TransactionRepositoryShould.java
@@ -15,22 +15,32 @@ public class TransactionRepositoryShould {
 
   @Mock
   Clock clock;
- public static final String TODAY="12/05/2020";
+  public static final String TODAY = "12/05/2020";
   private TransactionRepository transactionRepository;
 
   @Before
   public void initialise() {
     transactionRepository = new TransactionRepository(clock);
+    given(clock.todayAsString()).willReturn(TODAY);
   }
 
   @Test
   public void
   create_and_store_a_deposit_transaction() {
-    given(clock.todayAsString()).willReturn(TODAY);
     transactionRepository.addDeposite(100);
     List<Transaction> transactions = transactionRepository.allTransactions();
     assertEquals(transactions.size(), 1);
     assertEquals(transactions.get(0), (transaction(TODAY, 100)));
+
+  }
+
+  @Test
+  public void
+  create_and_store_a_withdrawal_transaction() {
+    transactionRepository.addWithdrawal(100);
+    List<Transaction> transactions = transactionRepository.allTransactions();
+    assertEquals(transactions.size(), 1);
+    assertEquals(transactions.get(0), (transaction(TODAY, -100)));
 
   }
 
